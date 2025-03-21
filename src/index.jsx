f1bb94e9b487e3d1d0d8068066285629e6ed4a8b
goldenmemories <sunmiaodi@gmail.com>
2025-03-21 12:27:26 -0600

    Update index.jsx
    Switch the app from App to the desktop version

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import TenantManagementSystem from './TenantManagementSystem';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TenantManagementSystem />
   </React.StrictMode>
 );
