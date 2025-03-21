88653e907280b04463d622b2df2f52c14de7bd4c
goldenmemories <sunmiaodi@gmail.com>
2025-03-21 12:05:05 -0600

    Update TenantManagementSystem.jsx

--- src/TenantManagementSystem.jsx
+++ src/TenantManagementSystem.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Alert, AlertDescription } from '@/components/ui/alert';
+import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
+import { Alert, AlertDescription } from './components/ui/alert';
 import { 
   Calendar, Wrench, DollarSign, Users, Mail, 
   Edit, X, Plus, Archive, Menu, ChevronDown, ChevronUp 
