2ddb6e51ddbaa449d8bd6df4cca4f5e063eeeedd
goldenmemories <sunmiaodi@gmail.com>
2025-03-21 11:34:41 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TenantManagementSystem from './TenantManagementSystem';
+
+function App() {
+  return (
+    <div className="App">
+      <TenantManagementSystem />
+    </div>
+  );
+}
+
+export default App;
